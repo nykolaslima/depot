794f0eb0e482cba243c4b5f8eb7125c702a4a08a
Nykolas Laurentino de Lima <nykolas.lima@gmail.com>
2013-04-08 15:30:08 -0300

    chapter 8 - adding cart to allow users to add products to be purchased

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
+    counter = session[:counter] || 0
+    session[:counter] = counter + 1
     @products = Product.order(:title)
   end
 end
